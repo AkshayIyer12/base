7580fcfd8e4f39f3ed1f371ad8fd9ceb2c60410a
Joshpitzalis <joshpitzalis@gmail.com>
2018-09-03 18:24:53 +0530

    hot module replacement

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+let root =   document.getElementById('root')
+let render = () => {
+  ReactDOM.render( <App />, root)
+}
+
+if (module.hot){
+  module.hot.accept('./App', () => {
+    setTimeout(render)
+  })
+}
+
+render()
 registerServiceWorker();
